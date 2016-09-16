889f5e497cb254641294aeb5c5c0485146312337
jeromylui <luijeromy@gmail.com>
2016-09-15 22:40:45 -0700

    Update simplesolver.py

--- simplesolver.py
+++ simplesolver.py
@@ -1,13 +1,12 @@
 class gameTree:
-	def __init__(self, position, move=None, parent = None, branches=[]):
+	def __init__(self, position, parent = None, branches=[]):
 		self.position = position
 		for branch in branches:
 			assert isinstance(branch, gameTree)
 		self.parent = parent
 		self.branches = branches
 		self.status = "Undecided"
-		self.move = move
-		self.turn = True #True if curr player's turn. False otherwise.
+		self.turn = False #True if curr player's turn. False otherwise.
 
 	def __repr__(self):
 		if self.branches:
@@ -24,8 +23,15 @@ class gameTree:
 	def getBranches(self):
 		return self.branches
 
+	def getTurn(self):
+		return self.turn
+
+	def hasParent(self):
+		return self.parent != None
+
 	def addBranch(self, branch):
 		assert isinstance(branch, gameTree)
+		self.branches += [branch]
 
 	def changeBranches(self, branches = []):
 		for branch in branches:
@@ -36,17 +42,13 @@ class gameTree:
 	def getStatus(self):
 		return self.status
 
-	def gameMove(self, moved):
-		self.turn = not self.turn
-		return self.move(self.position, moved)
-
 	def changeStatus(self, status):
 		assert status == "Win" or status == "Loss" or status == "Tie" or status == "Undecided"
 		self.status = status
 
 #----------------------------------------------------
 def initial_position():
-	return 3
+	return 4
 
 def primitive(pos):
 	if pos <= 0:
@@ -56,30 +58,40 @@ def primitive(pos):
 
 def gen_moves(pos):
 	possible = []
+	if pos-2 >=0 :
+		possible += [2]
 	if pos-1 >= 0:
 		possible += [1]
-	if pos-2 >0 :
-		possible += [2]
 	return possible
 	
 def do_moves(pos, move):
 	return pos-move
 
 #----------------------------------------------------
-
 def simplesolver(init, prim, gen, do):
-	game = gameTree(init(), do)
-	possible_moves = gen(game.getPosition())
-	for pos in possible_moves: #[1,2]
-		if prim(game.getPosition() - pos):
-			if game.turn:
-				game.changeStatus("Win")
-			break
-		return simplesolver(lambda: game.gameMove(pos), prim, gen, do)
+	root = gameTree(init())
+	def helper(init, prim, gen, do, game):
+		nonlocal root
+		if not game:
+			game = gameTree(init())
+		print(game)
+		if game.parent:
+			game.turn = not game.parent.turn
+		if prim(game.getPosition()):
+			if game.getTurn():
+				return True
+			return False
+		possible_moves = gen_moves(game.getPosition())
+		if (len(possible_moves) > 1) and prim(do(game.getPosition(), possible_moves[0])):
+			possible_moves.pop()
+		game.changeBranches([gameTree(do(game.getPosition(), move), game) for move in possible_moves])
+		for branch in game.getBranches():
+			if helper(init, prim, gen, do, branch) and not game.getTurn():
+				root.changeStatus("Win")
+	helper(init, prim, gen, do, None)
+	if root.getStatus() == "Undecided":
+		root.changeStatus("Loss")
+	return root.getStatus()
 
-	if game.getStatus == "Undecided":
-		game.changeStatus("Loss")
-		
-	return game.getStatus()
 
 print(simplesolver(initial_position, primitive, gen_moves, do_moves))
