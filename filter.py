d6e23fa4a42bc89c7d2cdb3cf74fddde98a74e75
jeromylui <luijeromy@gmail.com>
2016-09-28 18:16:41 -0700

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,27 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+    data = [1,2,3,4,5]
+else:
+    data = []
+
+def func(x):
+	return x % 2==0
+
+data = (func(data), data)
+
+#Scatter
+data = comm.scatter(data, root=0)
+print "rank", rank, "recieved", data
+data = data + 2
+
+#Gather
+data = comm.gather(data, root=0)
+
+comm.Barrier()
+
+if rank == 0:
+    print data
