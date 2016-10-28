8d7aad06f0058225e570cf99412c631ab4cac6fd
jeromylui <luijeromy@gmail.com>
2016-10-28 16:17:39 -0700

    Create pi.py

---
+++ spark/pi.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext
+from random import uniform
+
+def sample():
+	x, y = random.uniform(0,1), random.uniform(0,1)
+	if (x*x + y*y < 1):
+		return 1
+	else:
+		return 0
+
+def sums(x,y):
+	return x + y
+
+sc = SparkContext("local", "Pi Problem 2")
+temp = sc.parallelize(range(0,1000000)).map(sample).reduce(sums)
+pi = (4*temp)/1000000
